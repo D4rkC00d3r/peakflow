e4ffc7a03aa542e3d3a2bd830d7cfdc2180957a0
Keith <thed4rkc00d3r@gmail.com>
2020-03-22 01:15:53 +0000

    Added forced user agreement.
    Added a dialog box to force the user to agree or disagree with the user notice.

--- app/src/main/java/com/example/peakflow/MainActivity.java
+++ app/src/main/java/com/example/peakflow/MainActivity.java
@@ -2,9 +2,8 @@ package com.example.peakflow;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.BroadcastReceiver;
-import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
 import android.content.res.AssetFileDescriptor;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
@@ -13,11 +12,10 @@ import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.SeekBar;
-import android.widget.Switch;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.karumi.dexter.Dexter;
@@ -39,9 +37,11 @@ public class MainActivity extends AppCompatActivity {
     int maxPeak;
     boolean muteAudio = false;
 
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // Make to run your application only in portrait mode
         setContentView(R.layout.activity_main);
         Dexter.withActivity(this)
                 .withPermissions(
@@ -53,8 +53,39 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {/* ... */}
         }).check();
+
+        userAgreement();
     }
 
+    public void userAgreement() {
+        // This is the initial user agreement that opens up every time the app starts. The user must agree before using the app.
+        AlertDialog.Builder userAgreement = new AlertDialog.Builder(this);
+        userAgreement.setTitle("User notice");
+        userAgreement.setMessage("Please do not take this app as medical advice. \n\nDo you accept?");
+        userAgreement.setCancelable(false);
+        //TODO create a log file with timestamp of acceptance.
+        userAgreement.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        userAgreement.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        finish();
+                        System.exit(0);
+                    }
+                });
+
+        AlertDialog alert11 = userAgreement.create();
+        alert11.show();
+    }
+    
 
     @SuppressLint("SetTextI18n")
     public void recordBlow(View view) {
@@ -86,6 +117,8 @@ public class MainActivity extends AppCompatActivity {
         AudioRecord ar = new AudioRecord(MediaRecorder.AudioSource.MIC, 8000, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT, minSize);
         short[] buffer = new short[minSize];
         ar.startRecording();
+
+        //TODO Look at this as it is blocking the main thread.
         while (recorder) {
             ar.read(buffer, 0, minSize);
             for (short s : buffer) {
