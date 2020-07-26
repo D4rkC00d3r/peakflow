af7eebaff051dcefdae75355837daca570aa905b
Keith <thed4rkc00d3r@gmail.com>
2020-07-26 02:07:52 +0100

    Added new icons and cleaned up code.

--- app/src/main/java/com/example/peakflow/MainActivity.java
+++ app/src/main/java/com/example/peakflow/MainActivity.java
@@ -10,6 +10,7 @@ import android.media.AudioRecord;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
@@ -32,6 +33,7 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    public boolean inTest = false;
     Boolean isblowing;
     List<Integer> peakValues = new ArrayList<Integer>();
     int maxPeak;
@@ -85,71 +87,92 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog alert11 = userAgreement.create();
         alert11.show();
     }
-    
+
 
     @SuppressLint("SetTextI18n")
     public void recordBlow(View view) {
 
-        if (!muteAudio) {
-            Log.d("Debug", "Audio has been muted");
-            AssetFileDescriptor afd = null;
-            try {
-                afd = getAssets().openFd("blowInstruction.mp3");
-            } catch (IOException e) {
-                e.printStackTrace();
+        if (!inTest) {
+            if (!muteAudio) {
+                Log.d("Debug", "Audio has been muted");
+                AssetFileDescriptor afd = null;
+                try {
+                    afd = getAssets().openFd("blowInstruction.mp3");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                MediaPlayer player = new MediaPlayer();
+                try {
+                    player.setDataSource(Objects.requireNonNull(afd).getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    player.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                player.start();
             }
-            MediaPlayer player = new MediaPlayer();
-            try {
-                player.setDataSource(Objects.requireNonNull(afd).getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                player.prepare();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            player.start();
-        }
 
-        boolean recorder = true;
-        int minSize = AudioRecord.getMinBufferSize(8000, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT);
-        AudioRecord ar = new AudioRecord(MediaRecorder.AudioSource.MIC, 8000, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT, minSize);
-        short[] buffer = new short[minSize];
-        ar.startRecording();
+            boolean recorder = true;
+            int minSize = AudioRecord.getMinBufferSize(8000, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT);
+            AudioRecord ar = new AudioRecord(MediaRecorder.AudioSource.MIC, 8000, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT, minSize);
+            short[] buffer = new short[minSize];
+            ar.startRecording();
 
-        //TODO Look at this as it is blocking the main thread.
-        while (recorder) {
-            ar.read(buffer, 0, minSize);
-            for (short s : buffer) {
-                if (Math.abs(s) > 27000)   //DETECT VOLUME (IF I BLOW IN THE MIC)
-                {
-                    int blow_value = Math.abs(s);
-                    Log.d("debug", "blown Value: " + blow_value);
-                    peakValues.add(blow_value);
-                    setContentView(R.layout.activity_main);
-                    ar.stop();
-                    recorder = false;
-                    isblowing = true;
+            //TODO Look at this as it is blocking the main thread.
+            while (recorder) {
+                ar.read(buffer, 0, minSize);
+                for (short s : buffer) {
+                    if (Math.abs(s) > 27000)   //DETECT VOLUME (IF I BLOW IN THE MIC)
+                    {
+                        int blow_value = Math.abs(s);
+                        Log.d("debug", "blown Value: " + blow_value);
+                        peakValues.add(blow_value);
+                        setContentView(R.layout.activity_main);
+                        ar.stop();
+                        recorder = false;
+                        isblowing = true;
+                    }
                 }
             }
+            isblowing = false;
+            Log.d("DEBUG", "Blowing not detected!");
+            Log.d("Debug", String.valueOf(peakValues));
+            maxPeak = Collections.max(peakValues);
+
+
+            String displayValue = String.valueOf(maxPeak);
+            TextView blowValue = findViewById(R.id.bValue);
+            blowValue.setText(displayValue.substring(0, displayValue.length() - 2));
+
+            int peekScaleNumber = Integer.parseInt(Integer.toString(maxPeak).substring(0, 1));
+
+
+            Log.d("Debug", "Peek Scale Value: " + peekScaleNumber);
+            final SeekBar seekBar = findViewById(R.id.seekBar);
+            seekBar.setProgress(peekScaleNumber + 2);
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    seekBar.setProgress(0);
+                    TextView t = findViewById(R.id.bValue);
+                    t.setText("");
+                    inTest = false;
+
+                }
+            }, 2500);
+
+            peakValues.clear();
+        } else {
+            Log.d("DEBUG", "Please wait conducting a test");
         }
-        isblowing = false;
-        Log.d("DEBUG", "Blowing not detected!");
-        Log.d("Debug", String.valueOf(peakValues));
-        maxPeak = Collections.max(peakValues);
+    }
 
-
-        String displayValue = String.valueOf(maxPeak);
-        TextView blowValue = findViewById(R.id.bValue);
-        blowValue.setText(displayValue.substring(0, displayValue.length() - 2));
-
-        int peekScaleNumber = Integer.parseInt(Integer.toString(maxPeak).substring(0, 1));
-
-
-        Log.d("Debug", "Peek Scale Value: " + peekScaleNumber);
-        SeekBar seekBar = findViewById(R.id.seekBar);
-        seekBar.setProgress(peekScaleNumber + 2);
-        peakValues.clear();
+    public void exitBtn(View view) {
+        Log.d("DEBUG", "Closing app.");
+        finishAndRemoveTask();
     }
 }
